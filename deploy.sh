e12b7df2f1be8d3f253face316481212a13d92ef
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-07-24 02:35:00 +0900

    feat: Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,14 +6,11 @@ echo "> [RUN] Build boot jar"
 echo "> [RUN] Build Dockerfile"
 docker build -t f1c-server .
 
-echo "> [RUN] Rename Previous Docker"
-docker rename f1c-server f1c-server-old
+echo "> [RUN] Stop Previous f1c-server"
+docker stop f1c-server
+
+echo "> [RUN] Remove Previous f1c-server"
+docker rm f1c-server
 
 echo "> [RUN] Run Docker"
 docker run -p 8080:8080 --name f1c-server -d f1c-server
-
-echo "> [RUN] Stop Previous f1c-server"
-docker stop f1c-server-old
-
-echo "> [RUN] Remove Previous f1c-server"
-docker rm f1c-server-old
