b3bb7f150b4fe968b6b89f6f12ef784c896d86ff
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-07-26 04:29:00 +0900

    feat: Update FileUtil
    - Edit String.asInputStream()

--- src/main/kotlin/me/f1c/util/FileUtil.kt
+++ src/main/kotlin/me/f1c/util/FileUtil.kt
@@ -1,11 +1,11 @@
 package me.f1c.util
 
-import java.io.File
+import org.springframework.core.io.ClassPathResource
 
 object FileUtil {
     private const val FILE_LOCATION = "data"
 
-    private fun String.asInputStream() = File("$FILE_LOCATION/$this").inputStream()
+    private fun String.asInputStream() = ClassPathResource("$FILE_LOCATION/$this").inputStream
 
     fun String.readText() = this.asInputStream().bufferedReader().readText()
 }
