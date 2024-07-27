cbb4320448846af3df44ccf2e6a30819870e7336
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-07-28 00:57:30 +0900

    feat: Update SessionController

--- src/main/kotlin/me/f1c/adapter/session/SessionControllerImpl.kt
+++ src/main/kotlin/me/f1c/adapter/session/SessionControllerImpl.kt
@@ -1,11 +1,14 @@
-package me.f1c.adapter
+package me.f1c.adapter.session
 
-import me.f1c.domain.SessionDto
-import me.f1c.domain.SessionService
-import me.f1c.port.SessionController
+import me.f1c.domain.session.SessionDto
+import me.f1c.domain.session.SessionService
+import me.f1c.port.session.SessionController
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -13,6 +16,10 @@ import org.springframework.web.bind.annotation.RestController
 class SessionControllerImpl(
     private val sessionService: SessionService,
 ) : SessionController {
+    @PutMapping("/up-to-date")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    override fun upToDate(): Int = sessionService.upToDate()
+
     @GetMapping
     override fun findAll(): List<SessionDto> = sessionService.findAll()
 
--- src/main/kotlin/me/f1c/port/session/SessionController.kt
+++ src/main/kotlin/me/f1c/port/session/SessionController.kt
@@ -1,8 +1,10 @@
-package me.f1c.port
+package me.f1c.port.session
 
-import me.f1c.domain.SessionDto
+import me.f1c.domain.session.SessionDto
 
 interface SessionController {
+    fun upToDate(): Int
+
     fun findAll(): List<SessionDto>
 
     fun findBySessionKey(sessionKey: Int): SessionDto?
