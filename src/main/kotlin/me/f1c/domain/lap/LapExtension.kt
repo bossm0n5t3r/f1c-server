a901644e29959712dbd7a5dc88f15e43a703d239
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-08-26 01:07:05 +0900

    feat: Add LapExtension.kt

---
+++ src/main/kotlin/me/f1c/domain/lap/LapExtension.kt
@@ -0,0 +1,21 @@
+package me.f1c.domain.lap
+
+fun LapEntity.toDto() =
+    LapDto(
+        meetingKey,
+        sessionKey,
+        driverNumber,
+        i1Speed,
+        i2Speed,
+        stSpeed,
+        dateStart.toString(),
+        lapDuration,
+        isPitOutLap,
+        durationSector1,
+        durationSector2,
+        durationSector3,
+        segmentsSector1List.takeIf { it.isNotBlank() }?.split(", ")?.map { it.takeIf { it != "null" }?.toInt() } ?: emptyList(),
+        segmentsSector2List.takeIf { it.isNotBlank() }?.split(", ")?.map { it.takeIf { it != "null" }?.toInt() } ?: emptyList(),
+        segmentsSector3List.takeIf { it.isNotBlank() }?.split(", ")?.map { it.takeIf { it != "null" }?.toInt() } ?: emptyList(),
+        lapNumber,
+    )
