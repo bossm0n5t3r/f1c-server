42fac481862b3e4c61fcccfd2c45b4016e8d2cbf
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-09-20 05:19:00 +0900

    feat: Add Admin createSessionSummary API

--- src/main/kotlin/me/f1c/adapter/admin/AdminControllerImpl.kt
+++ src/main/kotlin/me/f1c/adapter/admin/AdminControllerImpl.kt
@@ -2,9 +2,13 @@ package me.f1c.adapter.admin
 
 import me.f1c.domain.ResponseDto
 import me.f1c.domain.admin.AdminService
+import me.f1c.domain.chat.AiSessionSummaryDto
+import me.f1c.domain.summary.SummaryService
 import me.f1c.domain.toResponseDto
 import me.f1c.port.admin.AdminController
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -12,7 +16,13 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/admin")
 class AdminControllerImpl(
     private val adminService: AdminService,
+    private val summaryService: SummaryService,
 ) : AdminController {
     @GetMapping("/up-to-date")
     override fun upToDate(): ResponseDto<Unit> = adminService.upToDate().toResponseDto()
+
+    @PostMapping("/sessions/{sessionKey}")
+    override fun createSessionSummary(
+        @PathVariable sessionKey: Int,
+    ): ResponseDto<AiSessionSummaryDto> = summaryService.createSessionSummary(sessionKey).toResponseDto()
 }
--- src/main/kotlin/me/f1c/port/admin/AdminController.kt
+++ src/main/kotlin/me/f1c/port/admin/AdminController.kt
@@ -1,7 +1,10 @@
 package me.f1c.port.admin
 
 import me.f1c.domain.ResponseDto
+import me.f1c.domain.chat.AiSessionSummaryDto
 
 interface AdminController {
     fun upToDate(): ResponseDto<Unit>
+
+    fun createSessionSummary(sessionKey: Int): ResponseDto<AiSessionSummaryDto>
 }
