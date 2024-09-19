14c9fbd8904c70beb4d4dc2108316c77f7d6cc7d
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-09-20 05:53:30 +0900

    feat: Update AiSessionSummaryDto

--- src/main/kotlin/me/f1c/domain/chat/AiSessionSummaryDto.kt
+++ src/main/kotlin/me/f1c/domain/chat/AiSessionSummaryDto.kt
@@ -1,16 +1,9 @@
 package me.f1c.domain.chat
 
-import com.fasterxml.jackson.module.kotlin.readValue
-import me.f1c.util.ObjectMapperUtil
-
 data class AiSessionSummaryDto(
     val id: Long,
     val sessionKey: Int,
     val prompt: String,
     val summary: String,
     val revision: Int,
-) {
-    val parsedSummary: List<String> by lazy {
-        ObjectMapperUtil.objectMapper.readValue<List<String>>(summary)
-    }
-}
+)
