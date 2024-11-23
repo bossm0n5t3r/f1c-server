020fc0a4aa00fb7414082802033ccb930f72e932
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-11-23 23:41:00 +0900

    feat: Update F1CExceptionHandlingAdvice

--- src/main/kotlin/me/f1c/exception/F1CExceptionHandlingAdvice.kt
+++ src/main/kotlin/me/f1c/exception/F1CExceptionHandlingAdvice.kt
@@ -10,4 +10,8 @@ class F1CExceptionHandlingAdvice {
     @ExceptionHandler(F1CException::class)
     @ResponseBody
     fun handleException(e: F1CException): ResponseEntity<ErrorResponse> = ResponseEntity(e.errorResponse, e.httpStatus)
+
+    @ExceptionHandler(IllegalArgumentException::class)
+    fun handleIllegalArgumentException(e: IllegalArgumentException): ResponseEntity<ErrorResponse> =
+        ResponseEntity(F1CBadRequestException(e).errorResponse, F1CBadRequestException(e).httpStatus)
 }
