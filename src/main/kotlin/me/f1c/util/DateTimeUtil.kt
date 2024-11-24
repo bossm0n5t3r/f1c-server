5711039d9753a0382889d3d05939bde86962337e
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-11-25 04:07:40 +0900

    feat: Update DateTimeUtil

--- src/main/kotlin/me/f1c/util/DateTimeUtil.kt
+++ src/main/kotlin/me/f1c/util/DateTimeUtil.kt
@@ -1,5 +1,6 @@
 package me.f1c.util
 
+import kotlinx.datetime.TimeZone
 import kotlinx.datetime.format.DateTimeComponents
 
 object DateTimeUtil {
@@ -7,4 +8,7 @@ object DateTimeUtil {
         DateTimeComponents.Formats.ISO_DATE_TIME_OFFSET
             .parse(this)
             .toLocalDateTime()
+
+    private const val SERVER_ZONE_ID = "Asia/Seoul"
+    val SERVER_TIME_ZONE = TimeZone.of(SERVER_ZONE_ID)
 }
