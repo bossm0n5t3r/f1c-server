84bd01939bdd6c15ce4bfe755529f31008d61875
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-12-01 10:50:40 +0900

    feat: Update Constants
    - Add START_ROUND, END_ROUND

--- src/main/kotlin/me/f1c/util/Constants.kt
+++ src/main/kotlin/me/f1c/util/Constants.kt
@@ -4,4 +4,7 @@ object Constants {
     const val REQUEST_ARROW = "->"
     const val RESPONSE_ARROW = "<-"
     const val CONFLICT_ARROW = "-><-"
+
+    const val START_ROUND = 1
+    const val END_ROUND = 24
 }
