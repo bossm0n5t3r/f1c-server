59e8339142c59885968e5b40ea9d01d6cf2f22de
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-12-15 21:41:30 +0900

    fix: Delete pit packages

--- src/main/kotlin/me/f1c/port/pit/PitController.kt
+++
@@ -1,7 +0,0 @@
-package me.f1c.port.pit
-
-import me.f1c.domain.ResponseDto
-
-interface PitController {
-    fun upToDate(sessionKey: Int): ResponseDto<Int>
-}
--- src/main/kotlin/me/f1c/port/pit/PitRepository.kt
+++
@@ -1,9 +0,0 @@
-package me.f1c.port.pit
-
-import me.f1c.domain.pit.PitDto
-
-interface PitRepository {
-    fun batchInsert(pitDtoList: List<PitDto>): Int
-
-    fun findAllBySessionKey(sessionKey: Int): List<PitDto>
-}
