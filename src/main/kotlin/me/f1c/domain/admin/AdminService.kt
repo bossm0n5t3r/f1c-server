179fa1385eeb93a9bef9cc5af231ef32cdbe02ad
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-12-15 20:17:30 +0900

    feat: Update AdminService

--- src/main/kotlin/me/f1c/domain/admin/AdminService.kt
+++ src/main/kotlin/me/f1c/domain/admin/AdminService.kt
@@ -1,60 +1,25 @@
 package me.f1c.domain.admin
 
-import me.f1c.configuration.LOGGER
-import me.f1c.configuration.LogResult
+import me.f1c.domain.chat.AiRaceResultSummaryDto
 import me.f1c.domain.circuit.CircuitService
 import me.f1c.domain.constructor.ConstructorService
 import me.f1c.domain.driver.DriverService
 import me.f1c.domain.lap.LapService
-import me.f1c.domain.position.PositionService
 import me.f1c.domain.result.RaceResultService
 import me.f1c.domain.schedule.RaceScheduleService
-import me.f1c.domain.session.SessionService
 import me.f1c.domain.summary.SummaryService
 import org.springframework.stereotype.Service
 
 @Service
 class AdminService(
-    private val sessionService: SessionService,
     private val driverService: DriverService,
-    private val positionService: PositionService,
-    private val summaryService: SummaryService,
     private val raceScheduleService: RaceScheduleService,
     private val raceResultService: RaceResultService,
     private val constructorService: ConstructorService,
     private val circuitService: CircuitService,
     private val lapService: LapService,
+    private val summaryService: SummaryService,
 ) {
-    fun upToDate() =
-        try {
-            sessionService.upToDate()
-
-            // Get All Session Keys
-            val allSessionKeys = sessionService.findAll().map { it.sessionKey }.distinct()
-            val updatedSessionKeyFromDriver = mutableListOf<Int>()
-            val updatedSessionKeyFromPosition = mutableListOf<Int>()
-            val updatedSessionKeyFromSummary = mutableListOf<Int>()
-            for (sessionKey in allSessionKeys) {
-                if (positionService.findAllBySessionKey(sessionKey).isEmpty()) {
-                    positionService.upToDate(sessionKey)
-                    updatedSessionKeyFromPosition.add(sessionKey)
-                }
-                if (summaryService.getSessionSummary(sessionKey).isEmpty()) {
-                    summaryService.createSessionSummary(sessionKey)
-                    updatedSessionKeyFromSummary.add(sessionKey)
-                }
-            }
-            LOGGER.info(
-                "${LogResult.SUCCEEDED} upToDate: {}, {}, {}",
-                updatedSessionKeyFromDriver.size,
-                updatedSessionKeyFromPosition.size,
-                updatedSessionKeyFromSummary.size,
-            )
-        } catch (e: Exception) {
-            LOGGER.error("${LogResult.FAILED} upToDate: {}, ", e.message, e)
-            throw e
-        }
-
     fun upToDateRaceSchedule(): Int = raceScheduleService.upToDate()
 
     fun upToDateRaceResult(): Int = raceResultService.upToDate()
@@ -65,8 +30,20 @@ class AdminService(
 
     fun upToDateCircuit(): Int = circuitService.upToDate()
 
+    fun upToDateAllLaps() = lapService.upToDateAll()
+
     fun upToDateLaps(
         season: Int,
         round: Int,
     ): Int = lapService.upToDate(season, round)
+
+    fun createRaceResultSummary(
+        season: Int,
+        round: Int,
+    ): AiRaceResultSummaryDto = summaryService.createRaceResultSummary(season, round)
+
+    fun updateRaceResultSummary(
+        season: Int,
+        round: Int,
+    ): AiRaceResultSummaryDto = summaryService.updateRaceResultSummary(season, round)
 }
