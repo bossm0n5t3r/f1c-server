46bedab49b8f811a5a44ebbf8dae0945922662df
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-12-15 21:43:30 +0900

    fix: Delete position packages

--- src/main/kotlin/me/f1c/port/position/PositionController.kt
+++
@@ -1,7 +0,0 @@
-package me.f1c.port.position
-
-import me.f1c.domain.ResponseDto
-
-interface PositionController {
-    fun upToDate(sessionKey: Int): ResponseDto<Int>
-}
--- src/main/kotlin/me/f1c/port/position/PositionRepository.kt
+++
@@ -1,9 +0,0 @@
-package me.f1c.port.position
-
-import me.f1c.domain.position.PositionDto
-
-interface PositionRepository {
-    fun batchInsert(positionDtoList: List<PositionDto>): Int
-
-    fun findAllBySessionKey(sessionKey: Int): List<PositionDto>
-}
