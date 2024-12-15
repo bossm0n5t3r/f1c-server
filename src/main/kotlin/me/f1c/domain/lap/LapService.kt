12c23c9a47814ea465f376eb58b225895b7088d4
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-12-15 19:47:00 +0900

    feat: Update LapService
    - Add upToDateAll

--- src/main/kotlin/me/f1c/domain/lap/LapService.kt
+++ src/main/kotlin/me/f1c/domain/lap/LapService.kt
@@ -1,19 +1,42 @@
 package me.f1c.domain.lap
 
+import kotlinx.datetime.Clock
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
 import me.f1c.adapter.external.JolpicaF1ClientImpl
 import me.f1c.configuration.LOGGER
 import me.f1c.configuration.LogResult
+import me.f1c.domain.UpToDateHelper
 import me.f1c.domain.jolpica.JolpicaF1ResponseDto
 import me.f1c.domain.jolpica.MRDataWithRaceTable
+import me.f1c.domain.jolpica.SeasonAndRound
 import me.f1c.port.external.callGet
 import me.f1c.port.lap.LapRepository
+import me.f1c.util.Constants.START_ROUND
 import org.springframework.stereotype.Service
 
 @Service
 class LapService(
     private val jolpicaF1Client: JolpicaF1ClientImpl,
     private val lapRepository: LapRepository,
+    private val upToDateHelper: UpToDateHelper,
 ) {
+    fun upToDateAll(): Int =
+        try {
+            val now = Clock.System.now().toLocalDateTime(TimeZone.UTC)
+            val season = now.year
+            val startSeasonAndRound = lapRepository.findLatest(season)?.toSeasonAndRound()?.inc() ?: SeasonAndRound(season, START_ROUND)
+            val (endSeasonAndRound, result) =
+                upToDateHelper.upToDate(now, startSeasonAndRound) { seasonAndRound, _ ->
+                    upToDate(seasonAndRound.season, seasonAndRound.round)
+                }
+            val totalCount = result.sum()
+            totalCount.also { LOGGER.info("{} upToDateAll: {}, {}, {}", LogResult.SUCCEEDED, startSeasonAndRound, endSeasonAndRound, it) }
+        } catch (e: Exception) {
+            LOGGER.info("{} upToDateAll: {}", LogResult.FAILED, e.message)
+            throw e
+        }
+
     fun upToDate(
         season: Int,
         round: Int,
