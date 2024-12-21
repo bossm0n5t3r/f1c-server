a9363b5bbc9f7102c9cbfffb64e78b8735906636
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-12-21 22:30:00 +0900

    feat: Update CryptoUtil

--- src/main/kotlin/me/f1c/util/CryptoUtil.kt
+++ src/main/kotlin/me/f1c/util/CryptoUtil.kt
@@ -1,14 +1,24 @@
 package me.f1c.util
 
+import com.nimbusds.jose.jwk.Curve
+import com.nimbusds.jose.jwk.ECKey
+import java.security.KeyFactory
 import java.security.KeyPair
 import java.security.KeyPairGenerator
+import java.security.PrivateKey
+import java.security.PublicKey
+import java.security.interfaces.ECPublicKey
 import java.security.spec.AlgorithmParameterSpec
 import java.security.spec.ECGenParameterSpec
 import java.security.spec.NamedParameterSpec
+import java.security.spec.PKCS8EncodedKeySpec
+import java.security.spec.X509EncodedKeySpec
 import kotlin.io.encoding.Base64
 import kotlin.io.encoding.ExperimentalEncodingApi
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
 
-@OptIn(ExperimentalEncodingApi::class)
+@OptIn(ExperimentalEncodingApi::class, ExperimentalUuidApi::class)
 object CryptoUtil {
     private const val ALGORITHM_EC = "EC"
     private const val ALGORITHM_ED25519 = "Ed25519"
@@ -27,8 +37,40 @@ object CryptoUtil {
 
     private fun generateED25519KeyPair(): KeyPair = generateKeyPair(ALGORITHM_ED25519, NamedParameterSpec.ED25519)
 
+    fun generateEncodedECKeyPair(): Pair<String, String> {
+        val keyPair = generateECKeyPair()
+        return Base64.encode(keyPair.public.encoded) to Base64.encode(keyPair.private.encoded)
+    }
+
     fun generateEncodedED25519KeyPair(): Pair<String, String> {
         val keyPair = generateED25519KeyPair()
         return Base64.encode(keyPair.public.encoded) to Base64.encode(keyPair.private.encoded)
+    }
+
+    fun Pair<String, String>.toKeyPair(): KeyPair {
+        val publicKey = this.first.toPublicKey(ALGORITHM_EC)
+        val privateKey = this.second.toPrivateKey(ALGORITHM_EC)
+        return KeyPair(publicKey, privateKey)
+    }
+
+    fun KeyPair.toECKey(): ECKey =
+        ECKey
+            .Builder(Curve.P_521, this.public as ECPublicKey)
+            .privateKey(this.private)
+            .keyID(Uuid.random().toString())
+            .build()
+
+    private fun String.toPublicKey(algorithm: String): PublicKey {
+        val keyBytes = Base64.decode(this)
+        val keySpec = X509EncodedKeySpec(keyBytes)
+        val keyFactory = KeyFactory.getInstance(algorithm)
+        return keyFactory.generatePublic(keySpec)
+    }
+
+    private fun String.toPrivateKey(algorithm: String): PrivateKey {
+        val keyBytes = Base64.decode(this)
+        val keySpec = PKCS8EncodedKeySpec(keyBytes)
+        val keyFactory = KeyFactory.getInstance(algorithm)
+        return keyFactory.generatePrivate(keySpec)
     }
 }
