d2da7f07d73efb8128130d793a531e2970b60d6e
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-12-22 18:43:00 +0900

    feat: Update JwtEncoderImpl

--- src/main/kotlin/me/f1c/security/JwtEncoderImpl.kt
+++ src/main/kotlin/me/f1c/security/JwtEncoderImpl.kt
@@ -3,12 +3,16 @@ package me.f1c.security
 import com.nimbusds.jose.jwk.source.ImmutableJWKSet
 import com.nimbusds.jose.jwk.source.JWKSource
 import com.nimbusds.jose.proc.SecurityContext
+import me.f1c.domain.token.TokenClientService
+import me.f1c.security.JwtProvider.ROLE
+import me.f1c.security.JwtProvider.SUB
 import org.springframework.security.oauth2.jwt.Jwt
 import org.springframework.security.oauth2.jwt.JwtEncoder
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters
 
 class JwtEncoderImpl(
     private val jwkSource: JWKSource<SecurityContext>,
+    private val tokenClientService: TokenClientService,
 ) : JwtEncoder {
     override fun encode(parameters: JwtEncoderParameters): Jwt {
         val ecKey =
@@ -18,7 +22,9 @@ class JwtEncoderImpl(
                 .first()
                 .toECKey()
         val headers = parameters.jwsHeader?.headers ?: emptyMap()
-        val claims = parameters.claims.claims
+        val clientId = parameters.claims.claims[SUB] as? String ?: ""
+        val tokenRole = tokenClientService.findByClientId(clientId)?.role ?: TokenRole.UNAUTHORIZED
+        val claims = parameters.claims.claims + mapOf(ROLE to tokenRole.role)
         val token =
             JwtProvider.createJWE(
                 ecKey,
