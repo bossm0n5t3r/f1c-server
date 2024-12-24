7dc3c0762a9073314d98e2370dbc9a28b158be1a
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-12-24 23:29:00 +0900

    feat: Update BatchControllerImpl

--- src/main/kotlin/me/f1c/adapter/batch/BatchControllerImpl.kt
+++ src/main/kotlin/me/f1c/adapter/batch/BatchControllerImpl.kt
@@ -1,11 +1,16 @@
 package me.f1c.adapter.batch
 
+import me.f1c.configuration.LOGGER
+import me.f1c.configuration.LogResult
 import me.f1c.domain.ResponseDto
 import me.f1c.domain.batch.BatchService
 import me.f1c.domain.batch.JobExecutionStatus
 import me.f1c.domain.batch.JobLaunchRequest
 import me.f1c.domain.toResponseDto
 import me.f1c.port.batch.BatchController
+import org.springframework.http.HttpMethod
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -20,4 +25,22 @@ class BatchControllerImpl(
     override fun executeJob(
         @RequestBody jobLaunchRequest: JobLaunchRequest,
     ): ResponseDto<JobExecutionStatus> = batchService.executeJob(jobLaunchRequest).toResponseDto()
+
+    private fun HttpMethod.log(params: Any? = null) {
+        LOGGER.info("{} test: {}, {}, {}", LogResult.SUCCEEDED, this, params, SecurityContextHolder.getContext().authentication)
+    }
+
+    @GetMapping("/test")
+    fun testGet(): ResponseDto<String> {
+        HttpMethod.GET.log()
+        return "test".toResponseDto()
+    }
+
+    @PostMapping("/test")
+    fun testPost(
+        @RequestBody dummyRequest: Any,
+    ): ResponseDto<Any> {
+        HttpMethod.POST.log(dummyRequest)
+        return dummyRequest.toResponseDto()
+    }
 }
